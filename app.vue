c69b3fa7e344b42f163dcf743d05eb0fb13eb45e
frasza <zan.fraas@gmail.com>
2023-11-12 22:46:14 +0100

    Init

---
+++ app.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+interface Comment {
+  id: number;
+  name: string;
+  body: string;
+}
+
+const comments = ref<Array<Comment> | null>();
+
+async function getData() {
+  try {
+    const { data } = await useFetch<Array<Comment>>(
+      "https://jsonplaceholder.typicode.com/comments",
+    );
+
+    comments.value = data.value;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const scrollEl = ref<HTMLElement | null>(null);
+useInfiniteScroll(scrollEl, async () => getData());
+
+onMounted(() => getData());
+</script>
+
+<template>
+  <div
+    v-if="comments && comments.length > 0"
+    class="h-screen w-screen flex justify-content">
+    <div ref="scrollEl" class="h-80 overflow-scroll">
+      <div v-for="item in comments" :key="item.id" class="flex flex-col">
+        <span>{{ item.name }}</span>
+        <span>{{ item.body }}</span>
+      </div>
+    </div>
+  </div>
+</template>
